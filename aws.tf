54aea2f26a79a26209091b7f3930d275e99e9304
Dmitry.Khomich <amidgcp@gmail.com>
2021-07-13 18:28:49 +0000

    del value

--- aws.tf
+++ aws.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = "~> 0.15.1"
   backend "s3" {
-    bucket = "tfstate-lambda-challenge"  # 4. Put your bucket here
+    bucket = "..."                 # 4. Put your bucket here
     key = "test"
-    region = "us-west-2"                 # 5. Put your region here
+    region = "..."                 # 5. Put your region here
   }
   required_providers {
     aws = {
